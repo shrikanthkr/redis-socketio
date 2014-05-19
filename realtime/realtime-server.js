b9f79e7323aac783adede1ea7bfec17134aad6cc
shrikanthkr <shrikanth_kr@yahoo.com>
2014-05-19 21:32:20 +0530

    remove duplicates by bringing redis publish outside of io conenct

--- realtime/realtime-server.js
+++ realtime/realtime-server.js
@@ -3,12 +3,12 @@ redis = require('redis').createClient(),
 redis_pub = require('redis').createClient();
 var socket;
 redis.subscribe('rt-change');
+
 io.on('connection', function(soc){
-	console.log('Got connection!!!!!!!');
-	socket=soc;
+	console.log(soc.id);
 });
 redis.on('message', function(channel, message){
-	console.log('Published!!!!!!!');
-	socket.emit('client-channel', JSON.parse(message));
+	io.sockets.emit('client-channel',JSON.parse(message));
 	redis_pub.publish('my_callback', JSON.parse(message));
+	console.log("Printing message");
 });
